b5fbb64bb25f482d78306bc46200c7faf83d7487
Gabriel Melo <gblsmlo@gmail.com>
2025-09-13 19:35:40 -0300

    config swagger

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,7 +1,27 @@
-import type { FastifyInstance } from "fastify";
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
 
-export function uploadImageRoute(server: FastifyInstance) {
-  server.post('/uploads', () => {
-    return 'Hello World'
-  })
+export const uploadImageRoute: FastifyPluginAsyncZod = async (server) => {
+	server.post(
+		'/uploads',
+		{
+			schema: {
+				summary: 'Upload an image',
+				tags: ['Uploads'],
+				body: z.object({
+					file: z.instanceof(Buffer).refine((file) => file.byteLength > 0, {
+						message: 'File is required',
+					}),
+				}),
+				response: {
+					201: z.object({ uploadId: z.uuid() }),
+					400: z.object({ message: z.string(), issues: z.any().optional() }),
+					500: z.object({ message: z.string() }),
+				},
+			},
+		},
+		(_request, reply) => {
+			return reply.status(201).send({ uploadId: crypto.randomUUID() })
+		},
+	)
 }
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,11 +1,14 @@
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
 	hasZodFastifySchemaValidationErrors,
+	jsonSchemaTransform,
 	serializerCompiler,
 	validatorCompiler,
 } from 'fastify-type-provider-zod'
-import { env } from '@/env'
 import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
@@ -28,9 +31,22 @@ server.setErrorHandler((error, _request, reply) => {
 
 server.register(fastifyCors, { origin: '*' })
 
-server.register(uploadImageRoute)
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'Upload Widget API',
+			description: 'API for the Upload Widget',
+			version: '0.1.0',
+		},
+	},
+	transform: jsonSchemaTransform,
+})
+server.register(fastifySwaggerUi, {
+	routePrefix: '/docs',
+})
 
-console.log(env.DATABASE_URL)
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
 	console.log('HTTP Server running!')
